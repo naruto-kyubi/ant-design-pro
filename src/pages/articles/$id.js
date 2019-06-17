5c6dff6d9b35d8661056cf2101ba519b2facded7
liuhy <25436136@qq.com>
2019-06-17 17:12:48 +0800

    new add like\star function.

--- src/pages/articles/$id.js
+++ src/pages/articles/$id.js
@@ -27,6 +27,20 @@ class Article extends PureComponent {
     });
 
     dispatch({
+      type: 'article/fetchArticleLikeById',
+      payload: {
+        targetId: id,
+      },
+    });
+
+    dispatch({
+      type: 'article/fetchArticleStarById',
+      payload: {
+        articleId: id,
+      },
+    });
+
+    dispatch({
       type: 'article/fetchCommentList',
       payload: {
         articleId: id,
@@ -35,7 +49,51 @@ class Article extends PureComponent {
     });
   }
 
-  onClick = () => {};
+  onClick = obj => {
+    const {
+      match: {
+        params: { id },
+      },
+      dispatch,
+    } = this.props;
+
+    const { type, selected } = obj;
+    if (type === 'like') {
+      if (!selected) {
+        dispatch({
+          type: 'article/addLike',
+          payload: {
+            type: 'article',
+            targetId: id,
+          },
+        });
+      } else {
+        dispatch({
+          type: 'article/deleteLike',
+          payload: {
+            type: 'article',
+            targetId: id,
+          },
+        });
+      }
+    } else if (type === 'star') {
+      if (!selected) {
+        dispatch({
+          type: 'article/addStar',
+          payload: {
+            articleId: id,
+          },
+        });
+      } else {
+        dispatch({
+          type: 'article/deleteStar',
+          payload: {
+            articleId: id,
+          },
+        });
+      }
+    }
+  };
 
   render() {
     const {
@@ -46,39 +104,51 @@ class Article extends PureComponent {
     } = this.props;
     const {
       articleDetail: { data },
+      like,
+      star,
     } = article;
     if (!data) {
       return null;
     }
 
-    const v = {
-      like: {
-        count: 88,
+    const { starCount, likeCount, commentCount } = data;
+    const v = [
+      {
+        type: 'like',
+        title: '喜欢',
+        count: likeCount,
+        selected: like.data ? true : false,
+        onClick: this.onClick,
+      },
+      {
+        type: 'star',
+        title: '收藏',
+        count: starCount,
+        selected: star.data ? true : false,
+        onClick: this.onClick,
+      },
+      {
+        type: 'message',
+        title: '评论',
+        count: commentCount,
         selected: false,
         onClick: this.onClick,
       },
-      star: {
-        count: 0,
-        selected: true,
-        onClick: this.onClick,
-      },
-
-      message: {
+      {
+        type: 'weibo',
+        title: '微博',
         count: 0,
         selected: false,
         onClick: this.onClick,
       },
-      weibo: {
+      {
+        type: 'wechat',
+        title: '微信',
         count: 0,
         selected: false,
         onClick: this.onClick,
       },
-      wechat: {
-        count: 0,
-        selected: false,
-        onClick: this.onClick,
-      },
-    };
+    ];
 
     return (
       <GridContent>
