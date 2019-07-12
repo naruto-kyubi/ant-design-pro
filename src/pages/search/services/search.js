66566828822ae76802ddea380a3f223f3c829e38
liuhy <25436136@qq.com>
2019-07-12 17:52:11 +0800

    modify request domain from naruto to server/api.

--- src/pages/search/services/search.js
+++ src/pages/search/services/search.js
@@ -2,9 +2,9 @@ import request from '@/utils/request';
 import { stringify } from 'qs';
 
 export async function searchall(payload) {
-  return request(`/naruto/v1/search/searchall?${stringify(payload)}`);
+  return request(`/server/api/v1/search/searchall?${stringify(payload)}`);
 }
 
 export async function searchlike(payload) {
-  return request(`/naruto/v1/search/likethis?${stringify(payload)}`);
+  return request(`/server/api/v1/search/likethis?${stringify(payload)}`);
 }
