cc786e24075b1214dad857cec291cff51f78cc41
qteamcheng <64435716@qq.com>
2019-07-13 11:41:14 +0800

    chang images proxy

--- config/config.js
+++ config/config.js
@@ -123,10 +123,10 @@ export default {
       changeOrigin: true,
       pathRewrite: { '^/server/api': '/server' },
     },
-    '/server/img': {
+    '/images': {
       target: 'http://localhost:8080',
       changeOrigin: true,
-      pathRewrite: { '^/server/img': '/server' },
+      pathRewrite: { '^/images': '/server/images' },
     },
   },
 };
