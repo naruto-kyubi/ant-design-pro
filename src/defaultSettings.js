a41e3136e3d49a44eec05295a91e34776ed05d2c
qteamcheng <64435716@qq.com>
2019-07-26 12:15:56 +0800

    chang default ico

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -12,7 +12,7 @@ module.exports = {
   menu: {
     disableLocal: false,
   },
-  title: 'Ant Design Pro',
+  title: 'AntDCommunity',
   pwa: true,
   // Your custom iconfont Symbol script Url
   // eg：//at.alicdn.com/t/font_1039637_btcrd5co4w.js
