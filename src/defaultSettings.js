9cf930a5b6b074febfa20ed0c0bcf944308be0d5
qteamcheng <64435716@qq.com>
2021-01-29 09:09:49 +0800

    修改为流式布局

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -2,10 +2,10 @@ module.exports = {
   // navTheme: 'dark', // theme for nav menu
   navTheme: 'light', // theme for nav menu
   primaryColor: '#1890FF', // primary color of ant design
-  // layout: 'sidemenu', // nav menu position: sidemenu or topmenu
-  layout: 'sidemenu', // nav menu position: sidemenu or topmenu
-  // contentWidth: 'Fluid', // layout of content: Fluid or Fixed, only works when layout is topmenu
-  contentWidth: 'Fixed',
+   layout: 'topmenu', // nav menu position: sidemenu or topmenu
+ // layout: 'sidemenu', // nav menu position: sidemenu or topmenu
+   contentWidth: 'Fluid', // layout of content: Fluid or Fixed, only works when layout is topmenu
+  // contentWidth: 'Fixed',
   fixedHeader: false, // sticky header
   autoHideHeader: false, // auto hide header
   fixSiderbar: false, // sticky siderbar
